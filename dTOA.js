855829ffbc8af6988b2655d85cf09888f2ef7fc2
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-10-11 16:56:46 -0500

    working on variables

---
+++ dTOA.js
@@ -0,0 +1,29 @@
+let a = 1;
+//a will be 1
+console.log(a)
+let b = a++;
+//b will be 2
+console.log(b)
+let c = ++a;
+console.log(c)
+//c will be 5
+// what is the value of a, b, and c?
+
+let d = "hello";
+let e = false;
+console.log(d)
+console.log(e)
+d++;
+e++;
+
+let perplexed; // perplexed is undefined (no value is assigned)
+perplexed + 2;
+console.log(perplexed + 2)
+
+let price = 2.7;
+price.toFixed(2);
+console.log(price)
+
+
+
+
