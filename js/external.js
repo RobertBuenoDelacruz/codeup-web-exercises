01078526ac3883337fa9054e4da3dce93268e6cd
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-10-12 16:57:27 -0500

    working on js exercises

---
+++ js/external.js
@@ -0,0 +1,33 @@
+console.log("Hello from external JavaScript")
+
+alert("Welcome to my Website!")
+prompt("What is your favorite color?")
+alert("Great, blue is my favorite color too!")
+
+alert("Renting a movie will be $3 everyday")
+prompt("How many days will you have the Little Mermaid?")
+let mermaid = parseInt(prompt("How many days will you have the Little Mermaid?"))
+prompt("How many days will you have Brother Bear?")
+prompt("How many days will you have Hercules?")
+
+
+
+
+
+
+
+// let movie = 3
+// let lm = movie * 3
+// let bb = movie * 5
+// let he = movie * 1
+// let total = lm + bb + he
+// console.log(total)
+//
+// let googlePerHour = 400
+// let amazonPerHour = 380
+// let facebookPerHour = 350
+// let googleHoursWorked = googlePerHour * 6
+// let amazonHoursWorked = amazonPerHour * 4
+// let facebookHoursWorked = facebookPerHour * 10
+// let totalPay = googleHoursWorked + amazonHoursWorked + facebookHoursWorked
+// console.log(totalPay)
