890128161f6cb154dc84bfcf16673b8e306c4508
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-10-13 10:45:07 -0500

    completed javascript with html exercises

--- js/external.js
+++ js/external.js
@@ -1,14 +1,45 @@
+"use strict";
 console.log("Hello from external JavaScript")
 
-alert("Welcome to my Website!")
-prompt("What is your favorite color?")
-alert("Great, blue is my favorite color too!")
+alert("Welcome to my Website!");
 
-alert("Renting a movie will be $3 everyday")
-prompt("How many days will you have the Little Mermaid?")
-let mermaid = parseInt(prompt("How many days will you have the Little Mermaid?"))
-prompt("How many days will you have Brother Bear?")
-prompt("How many days will you have Hercules?")
+let color = prompt("What is your favorite color?");
+alert(`Great, ${color} is my favorite color too!`);
+
+let daysWithLittleMermaid = parseInt(prompt("How many days will you have the Little Mermaid?"))
+alert(`You entered ${daysWithLittleMermaid}.`)
+
+let daysWithBrotherBear = parseInt(prompt("How many days will you have Brother Bear?"))
+alert(`You entered ${daysWithBrotherBear}.`)
+
+let daysWithHercules = parseInt(prompt("How many days will you have Hercules?"))
+alert(`You entered ${daysWithHercules}.`)
+
+let total = (daysWithHercules + daysWithBrotherBear + daysWithLittleMermaid) * 3
+//toFixed will add 2 decimal places, in this case you are using money.
+ alert(`Your total is: $${total.toFixed(2)}`)
+
+//-----------Payment Logic Problem---------------
+let googlePay = parseInt(prompt("How much does google pay you per hour?"))
+
+let facebookPay = parseInt(prompt("How much does facebook pay you per hour?"))
+
+let amazonPay = parseInt(prompt("How much does amazon pay you per hour?"))
+
+let googleHours = parseInt(prompt("How many hours did you work for google this week?"))
+
+let facebookHours = parseInt(prompt("How many hours did you work for facebook this week?"))
+
+let amazonHours = parseInt(prompt("How many hours did you work for amazon this week?"))
+
+let totalPay = (googlePay * googleHours) + (facebookPay * facebookHours) + (amazonPay * amazonHours)
+
+alert(`Your payment for this week will be $${totalPay.toFixed(2)}`)
+
+
+
+
+
 
 
 
