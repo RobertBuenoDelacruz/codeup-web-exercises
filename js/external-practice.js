74f10a85ce461b01339b4d91998e347e1a0474ad
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-10-14 23:45:06 -0500

    self practice with js external files

--- js/external-practice.js
+++
@@ -1,3 +0,0 @@
-alert ("You have entered my practice webpage!")
-let name = prompt ("What is you name?")
-prompt ("That is a really cool " + name)
---
+++ personal-study/js/external-practice.js
@@ -0,0 +1,3 @@
+alert("You have entered my practice webpage!")
+let name = prompt("What is you name?")
+alert(name + " sounds like a cool name!")
