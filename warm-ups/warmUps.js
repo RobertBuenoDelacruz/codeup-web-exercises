e51cd45b772dc03ccb9e731f1b422dfcb5cc7625
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-10-16 10:19:29 -0500

    capitalizeFirstLetter warm up

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -5,11 +5,23 @@ let year = "2013"
 console.log(`My name is ${name} and I graduated high school in ${year}.`)
 
 // const capitalizeFirstLetter = "codeup"
-// const capitlizeFirstLetter2 = (capitalizeFirstLetter.charAt(0)).toUpperCase()
-// console.log(capitlizeFirstLetter2);
+// const capitalizeFirstLetter2 = (capitalizeFirstLetter.charAt(0)).toUpperCase()
+// console.log(capitalizeFirstLetter2);
 
-function capitalizeFirstLetter(robert){
-    return robert.charAt(0).toUpperCase() + robert.slice(1)
+//Capitalizing first letter of a word
+// function capitalizeFirstLetter(robert){
+//     return robert.charAt(0).toUpperCase() + robert.slice(1)
+// }
+//
+// console.log(capitalizeFirstLetter("robert"))
+
+//Another way to do the method above ^^^
+const capitalizeFirstLetter = (str) => {
+    console.log(str);
+    let firstLetter = str.substring(0, 1);
+    let capitalizedFirstLetter = firstLetter.toUpperCase();
+    let restOfWord = str.substring(1);
+
+    return capitalizedFirstLetter + restOfWord;
 }
-
-console.log(capitalizeFirstLetter("robert"))
+console.log(capitalizeFirstLetter("testString"));
