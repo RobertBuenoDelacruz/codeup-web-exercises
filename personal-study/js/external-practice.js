fbbd9ca94a36dc0f0780b3f7fab710f6dfb9a4fa
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-10-15 21:41:01 -0500

    added age prompt

--- personal-study/js/external-practice.js
+++ personal-study/js/external-practice.js
@@ -1,3 +1,6 @@
 alert("You have entered my practice webpage!")
 let name = prompt("What is you name?")
 alert(name + " sounds like a cool name!")
+
+let myAge = prompt(`How old are you ${name}?`)
+alert(`${myAge}, wow that's old!`)
