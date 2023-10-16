68b2d05f02ffff2e435cd1adc03b1cb01d7324a8
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-10-16 09:30:49 -0500

    capitlizeFirstLetter warm up

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -4,3 +4,12 @@ let name = "Roberto Bueno"
 let year = "2013"
 console.log(`My name is ${name} and I graduated high school in ${year}.`)
 
+// const capitalizeFirstLetter = "codeup"
+// const capitlizeFirstLetter2 = (capitalizeFirstLetter.charAt(0)).toUpperCase()
+// console.log(capitlizeFirstLetter2);
+
+function capitalizeFirstLetter(codeup){
+    return codeup.charAt(0).toUpperCase() + codeup.slice(1)
+}
+
+let codeup = capitalizeFirstLetter() + codeup
