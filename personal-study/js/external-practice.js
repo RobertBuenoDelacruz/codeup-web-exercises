c87c4948565c26f3e7d9a4286524cf12dcf84a59
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-10-22 19:00:15 -0500

    more practice

--- personal-study/js/external-practice.js
+++ personal-study/js/external-practice.js
@@ -1,6 +1,14 @@
-alert("You have entered my practice webpage!")
-let name = prompt("What is you name?")
-alert(name + " sounds like a cool name!")
+// alert("You have entered my practice webpage!")
+// let name = prompt("What is you name?")
+// alert(name + " sounds like a cool name!")
+//
+// let myAge = prompt(`How old are you ${name}?`)
+// alert(`${myAge}, wow that's old!`)
 
-let myAge = prompt(`How old are you ${name}?`)
-alert(`${myAge}, wow that's old!`)
+function sayHello(){
+    console.log("Hello World!")
+}
+
+sayHello()
+
+//testing for self practice
