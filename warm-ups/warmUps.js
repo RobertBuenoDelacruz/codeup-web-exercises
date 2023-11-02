c7d9d9b529fcf28bba628318538785097dbda22c
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-11-02 09:48:59 -0500

    warm up 11/2

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -138,3 +138,44 @@ const knowsJavaScript = (arr) => {
 }
 console.log(knowsJavaScript(developers))
 
+//--------- Warm up 11/2 --------------
+// Create a function that accepts three arguments (arrOfElements, bgColor, textColor), and returns the array of objects with their corresponding properties changed to the inputted values of bgColor and textColor.
+
+let elements = [
+    {
+        el: "button",
+        style: {
+            backgroundColor: "white",
+            color: "black"
+        },
+        content: "Submit"
+    },
+    {
+        el: "p",
+        style: {
+            backgroundColor: "white",
+            color: "black"
+        },
+        content: "Our company is better at doing company things than your company."
+    },
+    {
+        el: "h2",
+        style: {
+            backgroundColor: "white",
+            color: "black"
+        },
+        content: "Welcome Back!"
+    }
+];
+
+function arrElements(arrOfElements, bgColor, textColor) {
+    for(let i = 0; i < arrOfElements.length; i++) {
+        arrOfElements[i].style.backgroundColor = bgColor;
+        arrOfElements[i].style.color = textColor;
+    }
+    return arrOfElements;
+}
+
+console.log(arrElements(elements, "purple", "grey"))
+
+
