9940e774cb7c1e05c586aad46d4f45ffcc2968a7
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-11-04 23:16:19 -0500

    finished workout exercise

--- personal-study/js/external-practice.js
+++ personal-study/js/external-practice.js
@@ -1,45 +1,48 @@
-// alert("You have entered my practice webpage!")
-// let name = prompt("What is you name?")
-// alert(name + " sounds like a cool name!")
+// // alert("You have entered my practice webpage!")
+// // let name = prompt("What is you name?")
+// // alert(name + " sounds like a cool name!")
+// //
+// // let myAge = prompt(`How old are you ${name}?`)
+// // alert(`${myAge}, wow that's old!`)
 //
-// let myAge = prompt(`How old are you ${name}?`)
-// alert(`${myAge}, wow that's old!`)
-
-function sayHello(){
-    console.log("Hello World!")
-}
-
-sayHello()
-
-//testing for self practice
-
-//The Fortune Teller
-function tellFortune(numOfChildren, partnersName, geographicLocation, jobTitle) {
-    return `I will be a ${jobTitle} in ${geographicLocation}, and married to ${partnersName} with ${numOfChildren} children.`
-}
-
-console.log(tellFortune(3, 'Julie', 'London', 'Web Developer'))
-
-//The Puppy Age Calculator
-function calculateDogAge(age)  {
-    let dogYears = 7 * age
-    console.log("Your dog is " + dogYears + " years old in dog years!")
-}
-calculateDogAge(2)
+// function sayHello(){
+//     console.log("Hello World!")
+// }
+//
+// sayHello()
+//
+// //testing for self practice
+//
+// //The Fortune Teller
+// function tellFortune(numOfChildren, partnersName, geographicLocation, jobTitle) {
+//     return `I will be a ${jobTitle} in ${geographicLocation}, and married to ${partnersName} with ${numOfChildren} children.`
+// }
+//
+// console.log(tellFortune(3, 'Julie', 'London', 'Web Developer'))
+//
+// //The Puppy Age Calculator
+// function calculateDogAge(age)  {
+//     let dogYears = 7 * age
+//     console.log("Your dog is " + dogYears + " years old in dog years!")
+// }
+// calculateDogAge(2)
 //-----------Weekly Workouts-----------
-let workOuts = [{
+let workOuts = {
     Monday: "Shoulders and Arms",
     Tuesday: "Legs",
     Wednesday: "Chest and Tri",
     Thursday: "Legs and Shoulders",
     Friday: "Back and Bi",
-    Saturday: "REST",
-    Sunday: "REST"
-}]
+    monday: "Shoulders and Arms",
+    tuesday: "Legs",
+    wednesday: "Chest and Tri",
+    thursday: "Legs and Shoulders",
+    friday: "Back and Bi"
+}
 
 function weeklyWorkouts(arrDayOfTheWeek) {
-    for(let i = 0; i < arrDayOfTheWeek.length; i++) {
-        console.log(workOuts)
-    }
+    return workOuts[arrDayOfTheWeek] || "There are no workouts scheduled for today."
 }
-weeklyWorkouts("Monday")
+let day = "tuesday";
+let result = weeklyWorkouts(day);
+console.log(result);
