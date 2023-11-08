9a7cc648e5fc6215a8c75d4ec840a23f7ab86c1e
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-11-08 09:38:39 -0600

    order-pizza html

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -178,4 +178,18 @@ function arrElements(arrOfElements, bgColor, textColor) {
 
 console.log(arrElements(elements, "purple", "grey"))
 
+//--------- Warm up 11/6 ------------
+//FizzBuzz********
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
+
 
