cdbe17303dbaf7c99b2d357a181ecda3db119920
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-11-28 15:54:12 -0600

    finsihed ajax exercise

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,15 @@
-function lastCommit (USERNAME) {
-
-    let url = `https://api.github.com/users/${USERNAME}/events/public`
-    fetch(url, //github API that will require a username you are target that is in a variable USERNAME
-        {headers: {"Authorization": `token ${GITHUB_API_KEY}`} //API key that will be hidden, so it won't be exposed when pushed to github since the real key is ignored
+function lastCommit (username, token) {
+    return fetch(`https://api.github.com/users/${username}/events/public`, //github API that will require a username you are target that is in a variable USERNAME
+        {headers: {"Authorization": `token ${token}`} //API key that will be hidden, so it won't be exposed when pushed to github since the real key is ignored
         })
-        .then(response => response.json()) //convert to json
-        .then(data => console.log(data[0].payload.commits[0])) //userData at index 0 to get the last commit and the date by also using .created_at
-        .catch(error => console.log("Error" + error))
+        .then(response => response.json())//convert to json
+        // .then(data => console.log(data[0].payload.commits[0])) //userData at index 0 to get the last commit and the date by also using .created_at
+        // .catch(error => console.log("Error" + error))
 }
-lastCommit('robertbuenodelacruz')
+lastCommit('robertbuenodelacruz', GITHUB_API_KEY)
+.then(data => {
+    console.log(data)
+    let commit = data.filter(event => event.types === "PushEvent")
+    console.log(commit);
+    console.log(new Date(commit[0].created_at))
+})
