7a7b9afd46708e88d7db806d3c9eca2fb01b2bdb
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-12-04 12:06:46 -0600

    dice roller lec

---
+++ js/dice.js
@@ -0,0 +1,13 @@
+"use strict"
+//import functionName from "file path"
+import randomNumber from "./numbers-utils.js";
+export function rollD6(){
+    return randomNumber(1, 6);
+}
+
+export function rollD20(){
+    return randomNumber(1, 20);
+}
+
+// export { rollD6, rollD20}
+
